f3d5f57e3e28e68d105a003b9b53a43eddb94041
Yoinker Doinker <37874779+YoinkerDoinker@users.noreply.github.com>
2021-09-25 17:11:31 -0400

    more work on nav

--- app/src/main/java/com/example/exploreapp/FragmentActivity.kt
+++ app/src/main/java/com/example/exploreapp/FragmentActivity.kt
@@ -2,11 +2,39 @@ package com.example.exploreapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.exploreapp.fragments.MainFragment
+import com.example.exploreapp.fragments.secondFragment
+import com.example.exploreapp.fragments.thirdFragment
+import kotlinx.android.synthetic.main.activity_main.*
 
 class FragmentActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_fragment)
+
+        val firstFragment= MainFragment()
+        val secondFragment= secondFragment()
+        val thirdFragment= thirdFragment()
+
+        setCurrentFragment(firstFragment)
+
+        bottomNavigationView.setOnNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.home->setCurrentFragment(firstFragment)
+                R.id.person->setCurrentFragment(secondFragment)
+                R.id.settings->setCurrentFragment(thirdFragment)
+
+            }
+            true
+        }
+
     }
+
+    private fun setCurrentFragment(fragment:Fragment)=
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.flFragment,fragment)
+            commit()
+        }
+
 }
---
+++ app/src/main/java/com/example/exploreapp/fragments/SecondFragment.kt
@@ -0,0 +1,8 @@
+package com.example.exploreapp.fragments
+
+import androidx.fragment.app.Fragment
+import com.example.exploreapp.R
+
+class secondFragment : Fragment(R.layout.fragment_second) {
+
+}
